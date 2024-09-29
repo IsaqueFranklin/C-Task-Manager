8de92443fb07df01b47957f57a2cca9f79ded2f0
Isaque Franklin <isaquefrankli@gmail.com>
2024-09-28 22:52:30 -0300

    Changes

--- todo.c
+++ todo.c
@@ -1,7 +1,22 @@
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
+#include <leif/leif.h>
 
 int main() {
     glfwInit();
 
-    GLFWwindow* window = glfwCreateWindow(1200, 720, "Todo", NULL, NULL)
+    GLFWwindow* window = glfwCreateWindow(1200, 720, "Todo", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    return 0;
 }
